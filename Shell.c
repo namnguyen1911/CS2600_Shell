87c6effb4bc876c919f8892b5e934e62e1e4c2e1
namnguyen1911 <namnh11@gmail.com>
2021-12-04 20:57:36 +0000

    Add first codes

--- Shell.c
+++ Shell.c
@@ -1,1 +1,15 @@
+#include <stdio.h>
+
+int main(int argc, char **argv)
+{
+  // Load config files, if any.
+
+  // Run command loop.
+  lsh_loop();
+
+  // Perform any shutdown/cleanup.
+
+
+  return 0;
+}
 
